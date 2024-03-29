daaa6b834a009cf39ecc56dbe449f089e5caab2d
Modev <khalil.mouhammad@hotmail.fr>
2024-03-29 14:37:52 +0100

    ajout des pages pour la gesion des menus

---
+++ app/src/main/java/com/example/familycalandar/MenuAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.familycalandar
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+class MenuAdapter(private val menuList: List<MenuModel>, private val onMenuClickListener: (MenuModel) -> Unit) : RecyclerView.Adapter<MenuAdapter.MenuViewHolder>() {
+
+    // Fournit une référence aux vues pour chaque élément de données
+    class MenuViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var menuTitle: TextView = itemView.findViewById(R.id.menuTitle)
+        var menuDate: TextView = itemView.findViewById(R.id.menuDate)
+    }
+
+    // Crée de nouvelles vues (invoquées par le gestionnaire de layout)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_menu, parent, false)
+        return MenuViewHolder(itemView)
+    }
+
+    // Remplace le contenu d'une vue (invoquée par le gestionnaire de layout)
+    override fun onBindViewHolder(holder: MenuViewHolder, position: Int) {
+        val currentItem = menuList[position]
+        holder.menuTitle.text = currentItem.title
+        holder.menuDate.text = currentItem.date
+        holder.itemView.setOnClickListener {
+            onMenuClickListener(currentItem)
+        }
+    }
+
+    // Retourne la taille de votre liste de données
+    override fun getItemCount() = menuList.size
+}
---
+++ app/src/main/java/com/example/familycalandar/MenuViewActivity.kt
@@ -0,0 +1,32 @@
+package com.example.familycalandar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class MenuViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_view)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.menuRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val menuList = listOf(MenuModel("Menu 1", "Vendredi 12h"), MenuModel("Menu 2", "Samedi 20h"))
+
+        recyclerView.adapter = MenuAdapter(menuList) { menu ->
+            val intent = Intent(this, ViewMenuActivity::class.java)
+            startActivity(intent)
+        }
+
+        val addMenuButton: FloatingActionButton = findViewById(R.id.addMenuButton)
+        addMenuButton.setOnClickListener {
+            val intent = Intent(this, CreateMenuActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
