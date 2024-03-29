ebfba6a3072d818227f898965c44ca8be2babbb3
Modev <khalil.mouhammad@hotmail.fr>
2024-03-29 14:37:52 +0100

    ajout des pages pour la gesion des menus

---
+++ app/src/main/java/com/example/familycalandar/ViewMenuActivity.kt
@@ -0,0 +1,11 @@
+package com.example.familycalandar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_menu)
+    }
+}
