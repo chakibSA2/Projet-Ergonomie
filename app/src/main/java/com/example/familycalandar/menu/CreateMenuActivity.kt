9afef6a7c9318e1762c7928e6b67e6a465dfd20a
KakashiMiage <alison.palatuik@etu.univ-amu.fr>
2024-04-04 21:44:32 +0200

    Mid-design

--- app/src/main/java/com/example/familycalandar/menu/CreateMenuActivity.kt
+++ app/src/main/java/com/example/familycalandar/menu/CreateMenuActivity.kt
@@ -2,16 +2,25 @@ package com.example.familycalandar.menu
 
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.familycalandar.R
+import com.example.familycalandar.task.TasksListActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.util.Calendar
 
 class CreateMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_menu)
+
+        val validateButton: FloatingActionButton = findViewById(R.id.validate)
+        validateButton.setOnClickListener {
+            val intent = Intent(this, MenuViewActivity::class.java)
+            startActivity(intent)
+        }
 
         val DateButton: Button = findViewById(R.id.DateButton)
         DateButton.setOnClickListener {
